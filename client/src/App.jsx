e82e53a143dddb5e8c5c62c40bb1afd667a1db2d
RamakrishnaVenkat <91265544+RamakrishnaVenkat@users.noreply.github.com>
2024-01-28 04:23:59 +0530

    waste management on going

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-import Home from './views/home'
+// import Home from './views/home'
+// import Initiative from './Pages/Initiative/Initiative'
+import Navbar from './components/Navbar'
+import Wastemngment from './Pages/wasteManagement/Wastemngment'
+// import Chooseinitiative from './Pages/Initiative/Chooseinitiative'
 function App() {
 
 
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <Navbar/>
+      {/* <Initiative/> */}
+
+      {/* <Chooseinitiative/> */}
+      <Wastemngment/>
+      
     </>
   )
 }
